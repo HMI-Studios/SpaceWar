a006cae4c25fdb6721925f80e91693db4d99b0f2
kjniemela <johannes@niemela.se>
2024-01-22 18:25:01 -0600

    ships can collide, resulting in a draw

--- Game.gd
+++ Game.gd
@@ -1,11 +1,27 @@
 extends Node2D
 
 
+var is_draw = false
+
+
 func _ready():
     pass
+    
+    
+func _physics_process(delta):
+    if %Player1 and %Player2:
+        var player_distance = (%Player1.position - %Player2.position).length()
+        if player_distance < 15:
+            %Player1.kill()
+            %Player2.kill()
+            is_draw = true
+            %Timer.start()
+            await %Timer.timeout
+            get_tree().change_scene_to_file("res://Draw.tscn")
 
 
 func _on_player_1_died():
+    if is_draw: return
     %Timer.start()
     await %Timer.timeout
     get_tree().change_scene_to_file("res://Player2Win.tscn")
@@ -13,6 +29,7 @@ func _on_player_1_died():
 
 
 func _on_player_2_died():
+    if is_draw: return
     %Timer.start()
     await %Timer.timeout
     get_tree().change_scene_to_file("res://Player1Win.tscn")
