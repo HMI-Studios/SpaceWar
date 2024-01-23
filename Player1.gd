039804733331378b591be7b3af87b0e0f1c04e64
kjniemela <johannes@niemela.se>
2024-01-22 18:50:42 -0600

    add player 2 controls

--- Player1.gd
+++ Player1.gd
@@ -1,10 +1,6 @@
 extends "Ship.gd"
 
 
-func _ready():
-    pass # Replace with function body.
-
-
 func _physics_process(delta):
     if Input.is_action_pressed("player_1_fwd"):
         fwd()
--- Player2.gd
+++ Player2.gd
@@ -1,11 +1,17 @@
 extends "Ship.gd"
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-    pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-    pass
+func _physics_process(delta):
+    if Global.is_multiplayer:
+        if Input.is_action_pressed("player_2_fwd"):
+            fwd()
+        if Input.is_action_pressed("player_2_left"):
+            left(delta)
+        if Input.is_action_pressed("player_2_right"):
+            right(delta)
+        if Input.is_action_pressed("player_2_shoot"):
+            shoot()
+    else:
+        pass
+    
+    super._physics_process(delta)
